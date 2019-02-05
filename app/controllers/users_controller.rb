17c9a73c0a22d911c99ddb51b98b59028c0ba74b
omelinb <elros.ob@gmail.com>
2019-02-05 21:24:39 +0200

    Added params fetch; changed show and index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,30 @@
 # curl 'http://localhost:3000/users/index?coords=17.254522,2.434317'
-# curl 'http://localhost:3000/users/:id'
+# curl 'http://localhost:3000/users/6'
 
 class UsersController < ApplicationController
-  before_action :find_users, only: [:index]
+  before_action :find_users, only: :index
 
   RADIUS = 30
 
   # GET /users/?coords=latitude,longitude
   def index
-    render json: @users.to_json(only: [:id, :name, :surname, :gender])
+    render json: @users.select(:id, :name, :surname, :gender)
   end
 
   # GET /users/1
   def show
-    render json: User.select(
-      :id, :name, :surname, :age, :latitude, :longitude).find(params[:id])
+    user = User.find(params[:id])
+    render json: user.to_json(only: [:age, :latitude, :longitude])
   end
+
 
   private
     # Find users that are in the specific radius
     def find_users
-      @users = User.within(RADIUS, origin: params[:coords])
+      @users = User.within(RADIUS, origin: user_params)
+    end
+
+    def user_params
+      params.fetch(:coords)
     end
 end
